c4eca3e9f97ddc6643900fc04e48df0c79549029
daniel <danielyknechtli@gmail.com>
2023-04-01 17:16:23 -0700

    getting coordinates from geo location api

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-function getLocationOnMap() {
-  alert("here");
+let myLatLong = [];
+
+async function getLocationOnMap() {
+  if (navigator.geolocation) {
+    myLatLong = await getCoords(); // waiting on getCoords function to complete
+    alert(myLatLong[0] + ", " + myLatLong[1]);
+  } else {
+    document.getElementById("map").innerHTML =
+      "Geolocation is not supported by this browser.";
+  }
+}
+
+async function getCoords() {
+  let location = await new Promise((myResolve, myReject) => {
+    //navigator.geolocation.getCurrentPosition then passes control to the function showPosition
+    //a coordinates object is automatically passed to the showPosition function
+    navigator.geolocation.getCurrentPosition(myResolve);
+  });
+  return [location.coords.latitude, location.coords.longitude];
 }
